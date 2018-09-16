07ef79b2f3fcfce2817fb9d12b41d253e8667497
Ezugworie I.e <ezugworie.ikechukwu@live.com>
2018-09-16 14:51:19 -0500

    Started work on Order Collection

---
+++ client/store/Orders.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const GOT_ORDERS = 'GOT_ORDERS'
+const ORDER_ERROR = 'ORDER_ERROR'
+
+const gotOrders = orders => ({type: GOT_ORDERS, orders})
+const orderError = () => ({type: ORDER_ERROR})
+
+export const fetchOrders = ()
