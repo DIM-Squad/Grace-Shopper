77aa966869165e329502a1b4484333629e6c5d81
dustinheestand <heestand@post.harvard.edu>
2018-09-16 22:00:28 -0500

    killed extraneous comments

--- client/components/AddProductForm.js
+++ client/components/AddProductForm.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react'
-//import {Dropdown, Button} from 'semantic-ui-react'
 import {Form, Container, Icon, Dropdown} from 'semantic-ui-react'
-//import axios from 'axios'
 import {withRouter} from 'react-router-dom'
 import {addProduct} from '../store/products'
 import {connect} from 'react-redux'
