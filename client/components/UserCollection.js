6c11c77cc8829ef6668ef34e05c37db21f31c1b0
dustinheestand <heestand@post.harvard.edu>
2018-09-17 16:18:53 -0500

    built user list as table

--- client/components/UserCollection.js
+++ client/components/UserCollection.js
@@ -1,7 +1,7 @@
-import {Card, Button, Image, Divider} from 'semantic-ui-react'
-import React, {Component, Fragment} from 'react'
+import {Table, Divider, Container, Header} from 'semantic-ui-react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {withRouter} from 'react-router-dom'
+import {withRouter, NavLink} from 'react-router-dom'
 import {fetchUsers} from '../store/users'
 
 class UserCollection extends Component {
@@ -15,28 +15,35 @@ class UserCollection extends Component {
 
   render() {
     return (
-      <Fragment>
+      <Container>
         <Divider hidden />
-        <Card.Group stackable centered>
-          {this.props.users.map(user => (
-            <Card key={user.id}>
-              <Card.Content>
-                <Image size="medium" src={user.imageUrl} style={styles.image} />
-                <Card.Header>{user.fullName}</Card.Header>
-              </Card.Content>
-              <Card.Content extra>
-                <Button
-                  color="teal"
-                  floated="right"
-                  onClick={() => this.goToUser(user.id)}
-                >
-                  More details
-                </Button>
-              </Card.Content>
-            </Card>
-          ))}
-        </Card.Group>
-      </Fragment>
+        <Header as="h1" textAlign="left">
+          User List
+        </Header>
+        <Divider horizontal />
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Id</Table.HeaderCell>
+              <Table.HeaderCell>First Name</Table.HeaderCell>
+              <Table.HeaderCell>Surname</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {this.props.users.map(user => {
+              return (
+                <Table.Row key={user.id}>
+                  <Table.Cell>
+                    <NavLink to={`/users/${user.id}`}>{user.id}</NavLink>
+                  </Table.Cell>
+                  <Table.Cell>{user.firstName}</Table.Cell>
+                  <Table.Cell>{user.lastName}</Table.Cell>
+                </Table.Row>
+              )
+            })}
+          </Table.Body>
+        </Table>
+      </Container>
     )
   }
 }
@@ -46,12 +53,6 @@ const mapDispatchToProps = dispatch => ({
   fetchUsers: (filterType, filterId) =>
     dispatch(fetchUsers(filterType, filterId))
 })
-
-const styles = {
-  image: {
-    marginBottom: 20
-  }
-}
 
 export default withRouter(
   connect(mapStateToProps, mapDispatchToProps)(UserCollection)
