ceef04c955f39548a6446b8f6289fbcfb7f2611e
dustinheestand <heestand@post.harvard.edu>
2018-09-18 22:21:19 -0500

    updated cart store to stop reordering on quantity update

--- client/components/ProductDetail.js
+++ client/components/ProductDetail.js
@@ -11,7 +11,7 @@ import {
 } from 'semantic-ui-react'
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {withRouter, NavLink} from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 // User defined imports
 import {
   fetchSelectedProduct,
@@ -123,21 +123,10 @@ class ProductDetail extends Component {
                           />
                           <Item.Meta
                             content={
-                              selectedProduct.categories && (
-                                <div>
-                                  <span>In</span>
-                                  <span>
-                                    {selectedProduct.categories.map(c => (
-                                      <NavLink
-                                        to={`/categories/${c.id}`}
-                                        key={c.id}
-                                      >
-                                        {'  ' + c.name}
-                                      </NavLink>
-                                    ))}
-                                  </span>
-                                </div>
-                              )
+                              selectedProduct.categories &&
+                              selectedProduct.categories
+                                .map(c => c.name)
+                                .join(', ')
                             }
                           />
                           <Divider hidden />
